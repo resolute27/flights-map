5ec605fd8378e6528b13348402c3bb2bfc88f160
resolute27 <D1285475@o365.fcu.edu.tw>
2025-06-14 10:25:47 +0800

    initial

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,9 +1,23 @@
 export interface Flight {
 	id: number;
+	year: number;
+	month: number;
+	day: number;
+	dep_time: number;
+	sched_dep_time: number;
+	dep_delay: number;
+	arr_time: number;
+	sched_arr_time: number;
+	arr_delay: number;
 	carrier: string;
 	flight: number;
+	tailnum: string;
 	origin: string;
 	dest: string;
-	dep_time: number;
-	arr_time: number;
+	air_time: number;
+	distance: number;
+	hour: number;
+	minute: number;
+	time_hour: string;
+	name: string;
 }
